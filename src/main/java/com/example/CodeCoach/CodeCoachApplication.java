df0b626b6fa9e752477bd3082e3dde53530d7c92
Foordle <131844504+Foordle@users.noreply.github.com>
2025-11-13 10:24:57 +0900

    feat(project): initialize spring boot structure and add main view
    웹 프로젝트에 기본 스프링 부팅 파일 및 구성을 추가
    코드 평가 시스템에 대한 기본 UI 구조를 갖춘 메인 UI 구조를 구현(html기반, AI사용)

---
+++ src/main/java/com/example/CodeCoach/CodeCoachApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeCoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CodeCoach/CodeCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeCoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
